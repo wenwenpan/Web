0fa2aeeafa511af6b21770d37f3e3aed693e1a9c
wenwenpan <wenwen.yh.pan@gmail.com>
2018-11-17 22:39:31 +0800

    add ssl tcp
    Signed-off-by: wenwenpan <wenwen.yh.pan@gmail.com>

---
+++ SSLConsoleClient/SSLConsoleClient/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+namespace ConsoleAppClient
+{
+    public class SslTcpClient
+    {
+        private static Hashtable certificateErrors = new Hashtable();
+        // The following method is invoked by the RemoteCertificateValidationDelegate.
+        public static bool ValidateServerCertificate(
+                object sender,
+                X509Certificate certificate,
+                X509Chain chain,
+                SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }
+
+        public static void RunClient(string machineName)
+        {
+            // Create a TCP/IP client socket.
+            // machineName is the host running the server application.
+            TcpClient client = new TcpClient(machineName, 901);//与服务器连接
+            Console.WriteLine("Server Connected！{0} --> {1}",client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+            // Create an SSL stream that will close the client's stream.
+            SslStream sslStream = new SslStream(client.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+            // The server name must match the name on the server certificate.
+
+            X509Store store = new X509Store(StoreName.My);
+            store.Open(OpenFlags.ReadWrite);
+
+            //检索证书 
+            X509Certificate2Collection certs = store.Certificates.Find(X509FindType.FindBySubjectName, "MyClient", false); // vaildOnly = true时搜索无结果。
+
+            try
+            {
+                sslStream.AuthenticateAsClient("MyServer", certs, SslProtocols.Tls, true);//双向
+                DisplaySecurityLevel(sslStream);
+                DisplaySecurityServices(sslStream);
+                DisplayCertificateInformation(sslStream);
+                DisplayStreamProperties(sslStream);
+                //sslStream.AuthenticateAsClient("MyServer");//验证证书,单向
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+                return;
+            }
+
+            // Encode a test message into a byte array.
+            // Signal the end of the message using the "<EOF>".
+            byte[] messsage = Encoding.UTF8.GetBytes("Hello from the client.<EOF>");
+            // Send hello message to the server. 
+            sslStream.Write(messsage);
+            sslStream.Flush();
+            // Read message from the server.
+            string serverMessage = ReadMessage(sslStream);
+            Console.WriteLine("Server says: {0}", serverMessage);
+            // Close the client connection.
+            client.Close();
+            Console.WriteLine("Client closed.");
+        }
+
+        static string ReadMessage(SslStream sslStream)
+        {
+            // Read the  message sent by the server.
+            // The end of the message is signaled using the
+            // "<EOF>" marker.
+            byte[] buffer = new byte[8192];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                // Use Decoder class to convert from bytes to UTF8
+                // in case a character spans two buffers.
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                // Check for EOF.
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+            } while (bytes != 0);
+
+            return messageData.ToString();
+        }
+
+        static void DisplaySecurityLevel(SslStream stream)
+        {
+            Console.WriteLine("Cipher: {0} strength {1}", stream.CipherAlgorithm, stream.CipherStrength);
+            Console.WriteLine("Hash: {0} strength {1}", stream.HashAlgorithm, stream.HashStrength);
+            Console.WriteLine("Key exchange: {0} strength {1}", stream.KeyExchangeAlgorithm, stream.KeyExchangeStrength);
+            Console.WriteLine("Protocol: {0}", stream.SslProtocol);
+        }
+
+        static void DisplaySecurityServices(SslStream stream)
+        {
+            Console.WriteLine("Is authenticated: {0} as server? {1}", stream.IsAuthenticated, stream.IsServer);
+            Console.WriteLine("IsSigned: {0}", stream.IsSigned);
+            Console.WriteLine("Is Encrypted: {0}", stream.IsEncrypted);
+        }
+
+        static void DisplayStreamProperties(SslStream stream)
+        {
+            Console.WriteLine("Can read: {0}, write {1}", stream.CanRead, stream.CanWrite);
+            Console.WriteLine("Can timeout: {0}", stream.CanTimeout);
+        }
+
+        static void DisplayCertificateInformation(SslStream stream)
+        {
+            Console.WriteLine("Certificate revocation list checked: {0}", stream.CheckCertRevocationStatus);
+
+            X509Certificate localCertificate = stream.LocalCertificate;
+            if (stream.LocalCertificate != null)
+            {
+                Console.WriteLine("Local cert was issued to {0} and is valid from {1} until {2}.",
+                localCertificate.Subject,
+                    localCertificate.GetEffectiveDateString(),
+                    localCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Local certificate is null.");
+            }
+            X509Certificate remoteCertificate = stream.RemoteCertificate;
+            if (stream.RemoteCertificate != null)
+            {
+                Console.WriteLine("Remote cert was issued to {0} and is valid from {1} until {2}.",
+                    remoteCertificate.Subject,
+                    remoteCertificate.GetEffectiveDateString(),
+                    remoteCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Remote certificate is null.");
+            }
+        }
+
+        private static void DisplayUsage()
+        {
+            Console.WriteLine("To start the client specify:");
+            Console.WriteLine("clientSync machineName [serverName]");
+            Environment.Exit(1);
+        }
+    public static void Main(string[] args)
+        {
+            string machineName = null;
+            machineName = "127.0.0.1";//Server IP
+
+            try
+            {
+                RunClient(machineName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
+/*参考*/
+/*SslStream.AuthenticateAsClient Method  https://docs.microsoft.com/zh-cn/dotnet/api/system.net.security.sslstream.authenticateasclient?redirectedfrom=MSDN&view=netframework-4.7.2#overloads */
+/*
+ * public virtual void AuthenticateAsServer(X509Certificate serverCertificate, bool clientCertificateRequired, SslProtocols enabledSslProtocols, bool checkCertificateRevocation);
+ * bool clientCertificateRequired 该值指定是否向客户端请求证书用于进行身份验证
+ * X509Certificate serverCertificate 用于对服务器进行身份验证的 X509Certificate
+ */
---
+++ SSLConsoleExp/SSLConsoleExp/Program.cs
@@ -0,0 +1,177 @@
+﻿/*
+ * 服务器端代码 发送/接受字符串
+ * by @panwenwen
+ * 2018/11/17
+ */
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Text;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+       static X509Certificate serverCertificate = null;
+
+        public static void RunServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("0.0.0.0"), 901);//本机IP
+            listener.Start();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Waiting for a client to connect...");
+                    TcpClient client = listener.AcceptTcpClient();
+                    ProcessClient(client);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        static void ProcessClient(TcpClient client)
+        {
+            SslStream sslStream = new SslStream(client.GetStream(), false);
+            try
+            {
+                sslStream.AuthenticateAsServer(serverCertificate, true, SslProtocols.Tls, true);//发送证书
+                DisplaySecurityLevel(sslStream);
+                DisplaySecurityServices(sslStream);
+                DisplayCertificateInformation(sslStream);
+                DisplayStreamProperties(sslStream);
+
+                sslStream.ReadTimeout = 5000;
+                sslStream.WriteTimeout = 5000;
+                Console.WriteLine("Waiting for client message...");
+               string messageData = ReadMessage(sslStream);
+               Console.WriteLine("Received: {0}", messageData);
+                byte[] message = Encoding.UTF8.GetBytes("Hello from the server.");
+                Console.WriteLine("Sending hello message.");
+                sslStream.Write(message);
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                sslStream.Close();
+                client.Close();
+                return;
+            }
+            finally
+            {
+                sslStream.Close();
+                client.Close();
+            }
+        }
+
+        static string ReadMessage(SslStream sslStream)
+        {
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+            }
+            while (bytes != 0);
+
+            return messageData.ToString();
+        }
+
+        static void DisplaySecurityLevel(SslStream stream)
+        {
+            Console.WriteLine("Cipher: {0} strength {1}", stream.CipherAlgorithm, stream.CipherStrength);
+            Console.WriteLine("Hash: {0} strength {1}", stream.HashAlgorithm, stream.HashStrength);
+            Console.WriteLine("Key exchange: {0} strength {1}", stream.KeyExchangeAlgorithm, stream.KeyExchangeStrength);
+            Console.WriteLine("Protocol: {0}", stream.SslProtocol);
+        }
+
+        static void DisplaySecurityServices(SslStream stream)
+        {
+            Console.WriteLine("Is authenticated: {0} as server? {1}", stream.IsAuthenticated, stream.IsServer);
+            Console.WriteLine("IsSigned: {0}", stream.IsSigned);
+            Console.WriteLine("Is Encrypted: {0}", stream.IsEncrypted);
+        }
+
+        static void DisplayStreamProperties(SslStream stream)
+        {
+            Console.WriteLine("Can read: {0}, write {1}", stream.CanRead, stream.CanWrite);
+            Console.WriteLine("Can timeout: {0}", stream.CanTimeout);
+        }
+
+        static void DisplayCertificateInformation(SslStream stream)
+        {
+            Console.WriteLine("Certificate revocation list checked: {0}", stream.CheckCertRevocationStatus);
+
+            X509Certificate localCertificate = stream.LocalCertificate;
+            if (stream.LocalCertificate != null)
+            {
+                Console.WriteLine("Local cert was issued to {0} and is valid from {1} until {2}.",
+                localCertificate.Subject,
+                    localCertificate.GetEffectiveDateString(),
+                    localCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Local certificate is null.");
+            }
+            X509Certificate remoteCertificate = stream.RemoteCertificate;
+            if (stream.RemoteCertificate != null)
+            {
+                Console.WriteLine("Remote cert was issued to {0} and is valid from {1} until {2}.",
+                    remoteCertificate.Subject,
+                    remoteCertificate.GetEffectiveDateString(),
+                    remoteCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Remote certificate is null.");
+            }
+        }
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                X509Store store = new X509Store(StoreName.My);
+                store.Open(OpenFlags.ReadWrite);
+
+                // 检索证书 
+                X509Certificate2Collection certs = store.Certificates.Find(X509FindType.FindBySubjectName, "MyServer", false); // vaildOnly = true时搜索无结果。
+                if (certs.Count == 0) return;
+
+                serverCertificate = certs[0];
+                RunServer();
+                store.Close(); // 关闭存储区。
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
